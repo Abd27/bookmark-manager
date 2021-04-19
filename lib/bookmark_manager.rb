c6afbd87cf4046443e6452bb734d18b6b38d65b8
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-19 23:53:32 +0100

    use data base instead of hardcoded array

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,10 +1,10 @@
+require 'pg'
 class BookmarkManager
 
   def self.all  
-    @bookmarks = ['https://www.youtube.com/',
-                  'https://www.freecodecamp.org/',
-                  'https://www.gov.uk/'
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map { |bookmark| bookmark['url'] }
   end
 
 end
